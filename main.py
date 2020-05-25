4437b2f8f67deaf41c016e1f43002e2f7dbe8b92
Timofey Khegai <takhegay@edu.hse.ru>
2020-05-25 17:48:14 +0300

    Weather API

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Salut')
+import requests
+
+
+def getWeatherInfo(c_name):
+    weather_key = "9a65d9b34d2d2d7041856453c1c7c505"
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={c_name}&appid={weather_key}"
+    weather_response = requests.request("GET", weather_url)
+    weather_info = {
+        "weather_type": weather_response.json()["weather"][0]["main"],
+        "temperature": weather_response.json()["main"]["temp"],
+        "temperature_feels_like": weather_response.json()["main"]["feels_like"],
+        "pressure": weather_response.json()["main"]["pressure"],
+        "humidity": weather_response.json()["main"]["humidity"]
+    }
+    return weather_info
+
+
+print(getWeatherInfo("moscow"))
