5c9a423fb1d6e6ffda036cc92ed7e70a476c6ed8
Taran Alexei <alex48520@mail.ru>
2020-05-27 12:55:54 +0300

    General app structure

--- main.py
+++ main.py
@@ -15,4 +15,51 @@ def getWeatherInfo(c_name):
     return weather_info
 
 
-print(getWeatherInfo("moscow"))
+def mainMenu():
+    city_name = input("Enter a city name or \"exit\": ").strip().lower()
+    if city_name == "exit":
+        exit()
+    else:
+        print("Loading...\n")
+        cityMenu(city_name, 0, "0")
+
+
+def cityMenu(c_name, c_id, c_photo_link):
+    print("Options:\n"
+          "-City photo\n"
+          "-Weather\n"
+          "-Restaurants\n"
+          "-Attractions\n"
+          "-Hotels\n"
+          "-Back")
+    option = input("Enter an option: ").strip().lower()
+    print()
+    if option == "city photo":
+        cityMenu(c_name, c_id, c_photo_link)
+    elif option == "weather":
+        print("Loading...\n")
+        city_weather_info = getWeatherInfo(c_name)
+        print("Weather information:\n"
+              "Weather: " + city_weather_info["weather_type"] + "\n"
+              "Temperature: " + str(city_weather_info["temperature"]) + "\n"
+              "Temperature feels like: " + str(city_weather_info["temperature_feels_like"]) + "\n"
+              "Pressure: " + str(city_weather_info["pressure"]) + "\n"
+              "Humidity: " + str(city_weather_info["humidity"]) + "\n")
+        cityMenu(c_name, c_id, c_photo_link)
+    elif option == "restaurants":
+        print("Loading...\n")
+        cityMenu(c_name, c_id, c_photo_link)
+    elif option == "attractions":
+        print("Loading...\n")
+        cityMenu(c_name, c_id, c_photo_link)
+    elif option == "hotels":
+        print("Loading...\n")
+        cityMenu(c_name, c_id, c_photo_link)
+    elif option == "back":
+        mainMenu()
+    else:
+        print("There is no such option\n")
+        cityMenu(c_name, c_id, c_photo_link)
+
+
+mainMenu()
